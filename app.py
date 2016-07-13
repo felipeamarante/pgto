1ff9ad4a226d8594a0e0b8ecc4a5bce776fa715a
Felipe Amarante <felipeamarante@gmail.com>
2016-07-13 07:53:16 -0300

    adding minor error handling and security issues

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ app = Flask(__name__)
 
 
 
+@app.route("/")
+def usage():
+    return render_template('usage.html')
+
 @app.route("/<owerName>/<money>")
 def core(owerName, money):
 
     if not re.match("^[a-zA-Z]+$", owerName):
-        return render_template('errpage.html')
+        return render_template('errpage.html'), 500
     if not re.match("^[0-9]+$", money):
-        return render_template('errpage.html')
+        return render_template('errpage.html'), 500
 
     return render_template('fade2.html', owerName=owerName.title(), money=money)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
